43b68d9760fe65ece4fcf8c8c62dc5fc34fe1dee
LLEEVV2020 <lleevv2020@gmail.com>
2022-09-06 15:36:19 +0400

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ProjectManagementSystem from "./ProjectManagementSystem";
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProjectManagementSystem> 
-
-
-
-    </ProjectManagementSystem>
+    <ProjectManagementSystem> </ProjectManagementSystem>
   </React.StrictMode>
 );
 
