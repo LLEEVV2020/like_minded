10f60b4708b39f515180df5da359408df31e65dc
LLEEVV2020 <lleevv2020@gmail.com>
2022-09-06 20:02:05 +0400

    add

--- src/ProjectManagementSystem.js
+++ src/ProjectManagementSystem.js
@@ -1,7 +1,76 @@
+
+
 function ProjectManagementSystem(){
     return (
 
-        <div>Система управления проектами</div> 
+        
+
+        <div>
+            <h2>Система управления проектами</h2> 
+
+            <p>
+        Евгений Харитонов - 0 уровень <span style="{}">200 </span>
+    </p>
+
+{/*
+            <p>
+        Евгений Харитонов - 0 уровень <span style="color: red;">200 </span>баллов  <input type="text" value="100"></input><button>add</button>    
+    </p>
+    
+    <p>
+        Евгения - 0 уровень <span style="color: red;">100 </span>баллов <input type="text" value="100" /><button>add</button>   
+    </p>
+    
+    <p>
+        @IvanJoy_Cy - 0 уровень <span style="color: red;">100 </span>баллов <input type="text" value="100"/><button>add</button>   
+    </p>
+
+    <p>
+        @lewmilow - 0 уровень <span style="color: red;">100 </span>баллов <input type="text" value="100"/><button>add</button>   
+    </p>
+    
+    <p>
+        @neuromance - 0 уровень <span style="color: red;">100 </span>баллов <input type="text" value="100"/><button>add</button>   
+    </p>
+    
+    <p>
+        @Vl00l - 0 уровень <span style="color: red;">100 </span>баллов <input type="text" value="100"/><button>add</button>   
+    </p>
+
+    <br/>
+    ИТОГО <span style="color: red;">700 </span>
+
+    <br/>
+    <h2>Процент каждого</h2>
+    <p>
+        Евгений Харитонов  - <span style="color: red;">??</span>%   
+    </p>
+    
+    <p>
+        Евгения  - <span style="color: red;">??</span>%    
+    </p>
+    
+    <p>
+        @IvanJoy_Cy  - <span style="color: red;">??</span>%  
+    </p>
+
+    <p>
+        @lewmilow  - <span style="color: red;">??</span>%     
+    </p>
+    
+    <p>
+        @neuromance  - <span style="color: red;">??</span>%   
+    </p>
+    
+    <p>
+        @Vl00l - <span style="color: red;">??</span>%   
+    </p>*/
+}
+
+            
+        </div> 
+
+        
 
     );
 }
